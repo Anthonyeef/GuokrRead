68723bec5e509b48ae5792159ed9bf4f67f58206
Anthonyeef <anthonyeef@gmail.com>
2015-12-07 23:52:33 +0800

    add Adapter

---
+++ app/src/main/java/io/github/anthonyeef/guokrread/adapter/ArticleRecyclerAdapter.java
@@ -0,0 +1,15 @@
+package io.github.anthonyeef.guokrread.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by anthonyeef on 12/7/15.
+ */
+public class ArticleRecyclerAdapter extends
+        RecyclerView.Adapter<ArticleRecyclerAdapter.ViewHolder> {
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final
+    }
+}
--- app/src/main/java/io/github/anthonyeef/guokrread/fragment/HomeFragment.java
+++ app/src/main/java/io/github/anthonyeef/guokrread/fragment/HomeFragment.java
@@ -3,6 +3,7 @@ package io.github.anthonyeef.guokrread.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +21,12 @@ public class HomeFragment extends Fragment {
                              savedInstanceState) {
         RecyclerView rv = (RecyclerView) inflater.inflate(
                 R.layout.home_content_fragment, container,false);
-        )
+        setupRecyclerView(rv);
+        return rv;
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.setAdapter(new SimpleStringr);
     }
 }
